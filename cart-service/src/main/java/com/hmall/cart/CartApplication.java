442c7e50a787c0170f633722779f5168fda74c9d
nerea <bonjourping@gmail.com>
2024-04-07 15:27:40 +0200

    openfeign log: 1. OpenFeign will only output logs if the log level in the package where the FeignClient is located is set to DEBUG 2. Additionally, it has four log levels: NONE, BASIC, HEADERS, FULL  - global config:@EnableFeignClients(defaultConfiguration = DefaultFeignConfig.class)  - specific config: @FeignClient(value = "item-service", configuration = DefaultFeignConfig.class)

--- cart-service/src/main/java/com/hmall/cart/CartApplication.java
+++ cart-service/src/main/java/com/hmall/cart/CartApplication.java
@@ -1,5 +1,6 @@
 package com.hmall.cart;
 
+import com.hmall.api.config.DefaultFeignConfig;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,7 +12,7 @@ import org.springframework.web.client.RestTemplate;
 @MapperScan("com.hmall.cart.mapper")
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableFeignClients(basePackages = "com.hmall.api.client")
+@EnableFeignClients(basePackages = "com.hmall.api.client", defaultConfiguration = DefaultFeignConfig.class)
 public class CartApplication {
     public static void main(String[] args) {
         SpringApplication.run(CartApplication.class, args);
