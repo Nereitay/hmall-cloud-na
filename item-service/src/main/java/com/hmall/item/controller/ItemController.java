9831ddedec0d2dc42a0675c6f17ed625fad8370c
nerea <bonjourping@gmail.com>
2024-04-08 23:34:11 +0200

    gateway-service: routes

--- item-service/src/main/java/com/hmall/item/controller/ItemController.java
+++ item-service/src/main/java/com/hmall/item/controller/ItemController.java
@@ -27,7 +27,9 @@ public class ItemController {
 
     @ApiOperation("分页查询商品")
     @GetMapping("/page")
-    public PageDTO<ItemDTO> queryItemByPage(PageQuery query) {
+    public PageDTO<ItemDTO> queryItemByPage(PageQuery query,
+                                            @RequestHeader(value = "headerName", required = false) String headerValue) {
+        System.out.println("Custom Header : " + headerValue);
         // 1.分页查询
         Page<Item> result = itemService.page(query.toMpPage("update_time", false));
         // 2.封装并返回
