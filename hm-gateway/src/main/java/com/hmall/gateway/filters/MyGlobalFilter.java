a52c3efb620fcbc418edcf289ccba42ebea98a71
nerea <bonjourping@gmail.com>
2024-04-09 23:27:36 +0200

    gateway-service: Filter (Global & Custom)

---
+++ hm-gateway/src/main/java/com/hmall/gateway/filters/MyGlobalFilter.java
@@ -0,0 +1,41 @@
+package com.hmall.gateway.filters;
+
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+@Component
+public class MyGlobalFilter implements GlobalFilter, Ordered {
+    /**
+     * Process the Web request and (optionally) delegate to the next {@code WebFilter}
+     * through the given {@link GatewayFilterChain}.
+     *
+     * @param exchange the current server exchange
+     * @param chain    provides a way to delegate to the next filter
+     * @return {@code Mono<Void>} to indicate when request processing is complete
+     */
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        System.out.println("headers = " + exchange.getRequest().getHeaders());
+        return chain.filter(exchange);
+    }
+
+    /**
+     * Get the order value of this object.
+     * <p>Higher values are interpreted as lower priority. As a consequence,
+     * the object with the lowest value has the highest priority (somewhat
+     * analogous to Servlet {@code load-on-startup} values).
+     * <p>Same order values will result in arbitrary sort positions for the
+     * affected objects.
+     *
+     * @return the order value
+     * @see #HIGHEST_PRECEDENCE
+     * @see #LOWEST_PRECEDENCE
+     */
+    @Override
+    public int getOrder() {
+        return 0;
+    }
+}
---
+++ hm-gateway/src/main/java/com/hmall/gateway/filters/PrintAnyGatewayFilterFactory.java
@@ -0,0 +1,57 @@
+package com.hmall.gateway.filters;
+
+import lombok.Data;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.OrderedGatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+/**
+ * 固定后缀： GatewayFilterFactory，方便配置使用
+ * 前缀 PrintAny 写在配置文件中
+ */
+@Component
+public class PrintAnyGatewayFilterFactory extends AbstractGatewayFilterFactory<PrintAnyGatewayFilterFactory.Config> {
+    @Override
+    public GatewayFilter apply(Config config) {
+       /* return (exchange, chain) -> {
+            // 编写过滤器逻辑
+            System.out.println("PrintAny filter 执行了");
+            // 放行
+            return chain.filter(exchange);
+        };*/
+
+        // 装饰类，带优先级
+        return new OrderedGatewayFilter((exchange, chain) -> {
+            String a = config.getA();
+            String b = config.getB();
+            String c = config.getC();
+            System.out.println("参数值：a = " + a);
+            System.out.println("参数值：b = " + b);
+            System.out.println("参数值：c = " + c);
+            System.out.println("PrintAny filter 执行了");
+            return chain.filter(exchange);
+        }, 1);
+    }
+
+    // 自定义配置属性，成员变量名称很重要，下面会用到
+    @Data
+    public static class Config {
+        private String a;
+        private String b;
+        private String c;
+    }
+
+    // 将变量名称依次返回，顺序很重要， 将来读取参数时需要按顺序获取
+    @Override
+    public List<String> shortcutFieldOrder() {
+        return List.of("a", "b", "c");
+    }
+
+    // 将Config字节码传递给父类，父类负责帮我们读取yaml配置
+    public PrintAnyGatewayFilterFactory() {
+        super(Config.class);
+    }
+}
