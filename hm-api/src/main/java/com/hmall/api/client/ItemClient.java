63cc4f83764f9868d6e9f71e03d8714f8785e503
nerea <bonjourping@gmail.com>
2024-04-13 13:46:35 +0200

    Spring cloud circuit breaker - retry circuitBreaker ratelimiter (not tested)

--- hm-api/src/main/java/com/hmall/api/client/ItemClient.java
+++ hm-api/src/main/java/com/hmall/api/client/ItemClient.java
@@ -3,6 +3,9 @@ package com.hmall.api.client;
 import com.hmall.api.client.fallback.ItemClientFallback;
 import com.hmall.api.dto.ItemDTO;
 import com.hmall.api.dto.OrderDetailDTO;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
+import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -19,5 +22,8 @@ public interface ItemClient { // @RequestMapping annotation not allowed on @Feig
     List<ItemDTO> queryItemByIds(@RequestParam("ids") Collection<Long> ids);
 
     @PutMapping("/items/stock/deduct")
+    @TimeLimiter(name = "tlApi")
+    @Retry(name = "retryApi")
+    @CircuitBreaker(name = "cbApi")
     void deductStock(@RequestBody List<OrderDetailDTO> items);
 }
